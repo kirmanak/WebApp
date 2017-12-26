3063863da6529869732516bd770d1c075a49ae11
Kirill Kamakin <kirmanak@gmail.com>
2017-12-26 21:30:53 +0300

    Previous deletion was unsuccessfull

--- src/main/js/components/PointList.js
+++ src/main/js/components/PointList.js
@@ -57,7 +57,7 @@ export default class PointList extends React.Component{
         return (
             <div>
                 Количество элементов на странице: <InputNumber onChange={this.handleInput} defaultValue={this.props.pageState.size} min={1} max={5}/>
-                <Table striped bordered responsive hover>
+                <Table striped bordered condensed responsive hover>
                     <thead>
                     <tr>
                         <th>X</th>
